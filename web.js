3cfa1898d3e523c420bd1a40a579ab71227a688d
Matias Podeley <ubuntu@ip-10-139-42-212.ec2.internal>
2013-07-07 15:24:15 +0000

    Updated web.js to read form index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var fs =require('fs');
 
 
 app.get('/', function(request, response) {
-  response.send(fs.ReadFileSync('index.html' ,'utf-8'));
+  response.send(fs.readFileSync('index.html' ,'utf-8'));
 });
 
 var port = process.env.PORT || 5000;
