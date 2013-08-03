2c6dc8598f48e88845bbac7d18b1d5effe47db2d
Matias Podeley <ubuntu@ip-10-236-135-212.ec2.internal>
2013-08-03 17:25:37 +0000

    Fixed bugs in index.html (fluid-row) and added alt attribute

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest=require('restler');
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -39,6 +41,15 @@ var assertFileExists = function(infile) {
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+
+
+var restUrl = function(url) {
+    return rest.get(url).on('complete', function(result) {
+fs.writeFileSync('fromUrl.html', result);});
+
+
+};
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -55,6 +66,8 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,7 +78,10 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>','Url')
         .parse(process.argv);
+
+    var createFile = restUrl(program.url);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
